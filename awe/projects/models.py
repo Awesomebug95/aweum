395155609e353e010368b28cbded0b05cb83b460
Vladimir <khodykin.volodymyr@yandex.ru>
2022-02-22 23:58:23 +0300

    created project model

--- awe/projects/models.py
+++ awe/projects/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.db.models import *
 
-# Create your models here.
+from users.models import User
+
+
+class Project(Model):
+    """
+    Model Project.
+    """
+    title = CharField(
+        max_length=150,
+        unique=True
+    )
+    description = CharField(
+        max_length=800
+    )
+    author = ForeignKey(
+        User,
+        on_delete=PROTECT,
+        related_name='projects',
+        verbose_name='Автор'
+    )
+    workers = ManyToManyField(
+        User,
+        blank=True,
+        related_name='workers',
+        verbose_name='Работники',
+        help_text='Выберите работников'
+    )
+    deadline = DateField(
+        blank=False
+    )
+    admins = ManyToManyField(
+        User,
+        related_name='admins',
+        verbose_name='Администраторы'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return self.title[:25]
